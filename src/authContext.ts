4cee34ad9b1458410c270bc4ce2e68374eb13767
Leo <l3omtz@gmail.com>
2020-03-27 00:20:59 -0400

    init

---
+++ src/authContext.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const AuthContext = React.createContext({
+    signinRedirectCallback: () => ({}),
+    logout: () => ({}),
+    signoutRedirectCallback: () => ({}),
+    isAuthenticated: () => ({}),
+    signinRedirect: () => ({}),
+    signinSilentCallback: () => ({}),
+    createSigninRequest: () => ({})
+});
---
+++ src/authProvider.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import UserManager from './userManager';
+import { IAuthProvider } from './IAuthProvider'
+import { AuthContext } from './authContext';
+
+export class AuthProvider extends React.Component<IAuthProvider> {
+    userManager;
+
+    constructor(props: IAuthProvider) {
+        super(props);
+        this.userManager = new UserManager(props.authConfig)
+    }
+
+    render() {
+        return (
+            <AuthContext.Provider value={this.userManager}>
+                {this.props.children}
+            </AuthContext.Provider>
+        );
+    }
+}
